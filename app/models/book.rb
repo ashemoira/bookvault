7d6f389346fba2d645f49d9572f1d02535380be1
Magnolist Mint <ashe.moira@gmail.com>
2018-10-24 22:50:09 +0900

    feat: add actions

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,11 +6,21 @@ class Book < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 200 }
   validates :author, presence: true, length: { maximum: 200 }
+  validates :page, numericality: true
 
   enumerize :category, in: {
       novel: '1',
       comic: '2',
       computer_technology: '3',
       business_money: '4'
+  }
+
+  enumerize :format, in: {
+      paper_book: '1',
+      kidle: '2',
+      pdf: '3',
+      epub: '4',
+      kobo: '5',
+      ebook: '6'
   }
 end
