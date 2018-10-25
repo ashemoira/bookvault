96021cdf7863c75306849ff03100d0cc1388b699
Magnolist Mint <ashe.moira@gmail.com>
2018-10-26 02:26:53 +0900

    feat: create bookshelf controller

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -1,13 +1,48 @@
 class BookshelvesController < ApplicationController
-  def edit
-  end
-
   def index
-  end
-
-  def new
+    @bookshelves = Bookshelf.all
   end
 
   def show
   end
+
+  def new
+    @bookshelf = Bookshelf.new
+  end
+
+  def edit
+  end
+
+  def create
+    @bookshelf = Bookshelf.new(bookshelf_params)
+
+    if @bookshelf.save
+      redirect_to @bookshelf, notice: '本棚の登録が完了しました'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @bookshelf.update(bookshelf_params)
+      redirect_to @bookshelf, notice: '本棚の更新が完了しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookshelf.destroy
+    redirect_to books_url, notice: '本棚の削除に成功しました'
+  end
+
+  private
+
+    def set_bookshelf
+      @bookshelf = Bookshelf.find(params[:id])
+    end
+
+    def book_params
+      params.require(:bookshelf).permit(:title)
+    end
 end
