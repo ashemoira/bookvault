03855d91d83af8df57a3405fd267465206e685d2
Magnolist Mint <ashe.moira@gmail.com>
2018-10-31 01:26:17 +0900

    feat: modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookshelves/edit'
-  get 'bookshelves/index'
-  get 'bookshelves/new'
-  get 'bookshelves/show'
+  resources :bookshelves
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
