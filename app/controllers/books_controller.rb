586c04e2dd0e98b6136df8d4e0249820ad3c1ad7
Magnolist Mint <ashe.moira@gmail.com>
2018-10-31 02:23:37 +0900

    feat: modify book controll

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,21 @@
-class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /books
+# book controller controller
+class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update destroy]
+
   def index
     @books = Book.all
   end
 
-  # GET /books/1
-  def show
-  end
+  def show; end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /books
   def create
     @book = Book.new(book_params)
 
@@ -30,7 +26,6 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1
   def update
     if @book.update(book_params)
       redirect_to @book, notice: 'Book was successfully updated.'
@@ -39,20 +34,18 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1
   def destroy
     @book.destroy
     redirect_to books_url, notice: 'Book was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def book_params
-      params.require(:book).permit(:title, :author, :category, :page, :format)
-    end
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :category, :page, :format)
+  end
 end
